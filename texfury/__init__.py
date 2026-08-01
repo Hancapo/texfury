ccb5c9f227ecbf22daf9231febb6fa0839b8c02b
MrVicho13 <vicho0202@gmail.com>
2026-08-01 04:10:57 -0400

    Release 1.6.3

--- texfury/__init__.py
+++ texfury/__init__.py
@@ -4,44 +4,44 @@ from texfury.alpha import AlphaEdgeMipReport, AlphaEdgeReport
 from texfury.formats import BCFormat, MipFilter, RscCompression, suggest_format
 from texfury.texture import Texture
 from texfury.texture_dict import (
-    Game,
     ITD,
-    create_dict_from_folder,
+    Game,
     batch_convert,
+    create_dict_from_folder,
     extract_dict,
 )
 from texfury.utils import (
+    fit_dimensions,
     has_transparency,
+    image_dimensions,
     is_power_of_two,
     next_power_of_two,
     pot_dimensions,
-    fit_dimensions,
-    image_dimensions,
     resize_image,
     resize_image_to_max,
 )
 
-__version__ = "1.6.2"
+__version__ = "1.6.3"
 
 __all__ = [
+    "ITD",
     "AlphaEdgeMipReport",
     "AlphaEdgeReport",
     "BCFormat",
+    "Game",
     "MipFilter",
     "RscCompression",
-    "suggest_format",
     "Texture",
-    "Game",
-    "ITD",
-    "create_dict_from_folder",
     "batch_convert",
+    "create_dict_from_folder",
     "extract_dict",
+    "fit_dimensions",
     "has_transparency",
+    "image_dimensions",
     "is_power_of_two",
     "next_power_of_two",
     "pot_dimensions",
-    "fit_dimensions",
-    "image_dimensions",
     "resize_image",
     "resize_image_to_max",
+    "suggest_format",
 ]
